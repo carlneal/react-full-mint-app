d160984fed92acff39b1b0c08fda319da81fd024
Carlos Neal <carlos@omnisplayer.com>
2022-08-05 10:12:08 -0400

    Initial Commit

---
+++ scripts/deployRoboPunksNFT.js
@@ -0,0 +1,23 @@
+const hre = require("hardhat");
+
+async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+
+  const lockedAmount = hre.ethers.utils.parseEther("1");
+
+  const RoboPunksNFT = await hre.ethers.getContractFactory("RoboPunksNFT");
+  const roboPunksNFT = await RoboPunksNFT.deploy(unlockTime, { value: lockedAmount });
+
+  await roboPunksNFT.deployed();
+
+  console.log("RoboPunksNFT with 1 ETH deployed to:", roboPunksNFT.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
